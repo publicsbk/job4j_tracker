17a58901b6a5b7487dafa3b2ceab4de30aa6cac1
publicsbk <shebeko_pavel@mail.ru>
2022-02-20 14:07:45 +0300

    1.1. Создание объекта [#165452]

---
+++ src/main/java/ru/job4j/tracker/ru/job4j/oop/Dog.java
@@ -0,0 +1,10 @@
+package ru.job4j.tracker.ru.job4j.oop;
+
+public class Dog {
+
+    public static void main(String[] args) {
+        Dog polka = new Dog();
+        Dog sharik = new Dog();
+        Dog zhuchka = new Dog();
+    }
+}
