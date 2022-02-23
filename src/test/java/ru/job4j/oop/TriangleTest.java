065094b9c2fc1096d0cd78abbaba6801b2fa30ca
publicsbk <shebeko_pavel@mail.ru>
2022-02-23 11:16:27 +0300

    2.2. Рефакторинг - Площадь треугольника. [#122642 #267410]

--- src/test/java/ru/job4j/oop/TriangleTest.java
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -3,6 +3,7 @@ package ru.job4j.oop;
 import static org.hamcrest.Matchers.closeTo;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TriangleTest {
