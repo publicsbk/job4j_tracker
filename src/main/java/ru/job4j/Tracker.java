638df610f4bc84f32c9d34f6de4b4f97f5d797be
publicsbk <shebeko_pavel@mail.ru>
2022-02-25 18:30:23 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/Tracker.java
+++ src/main/java/ru/job4j/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -11,10 +13,27 @@ public class Tracker {
         return item;
     }
 
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] arr = new Item[size];
+        int counter = 0;
+        for (int i = 0; i < size; i++) {
+            if (key.equals(items[i].getName())) {
+                arr[counter] = items[i];
+                counter++;
+            }
+        }
+        arr = Arrays.copyOf(arr, counter);
+        return arr;
+    }
+
     public Item findById(int id) {
         Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
+        for (int i = 0; i < size; i++) {
+            Item item = items[i];
             if (item.getId() == id) {
                 rsl = item;
                 break;
