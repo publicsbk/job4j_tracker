08b25a15a7dfa3e586cc738d3e8a9f904ec280e1
publicsbk <shebeko_pavel@mail.ru>
2022-03-16 19:01:23 +0300

    9.1 Зависимость от System.out [#33568 #273265]

--- src/main/java/ru/job4j/tracker/CreateAction.java
+++ src/main/java/ru/job4j/tracker/CreateAction.java
@@ -22,4 +22,5 @@ public class CreateAction implements UserAction {
         out.println("Добавленная заявка: " + item);
         return true;
     }
+
 }
--- src/main/java/ru/job4j/tracker/FindByNameAction.java
+++ src/main/java/ru/job4j/tracker/FindByNameAction.java
@@ -27,4 +27,5 @@ public class FindByNameAction implements UserAction {
         }
         return true;
     }
+
 }
