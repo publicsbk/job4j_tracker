006448c278dd9214307c3a30f982c9b1dac45e13
publicsbk <shebeko_pavel@mail.ru>
2022-03-22 18:46:11 +0300

    0.4. Пользовательские исключения. [#219367]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -1,6 +1,7 @@
 package ru.job4j.ex;
 
 public class FindEl {
+
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
         for (int i = 0; i < value.length; i++) {
