8537f65ea4b33c5cdd914f5e83243a8855045c66
publicsbk <shebeko_pavel@mail.ru>
2022-04-11 18:23:49 +0300

    2. final [#334073]

--- src/main/java/ru/job4j/ooa/Airbus.java
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -34,5 +34,4 @@ public final class Airbus extends Aircraft {
                 + "name='" + name + '\''
                 + '}';
     }
-
 }
--- src/main/java/ru/job4j/ooa/Aircraft.java
+++ src/main/java/ru/job4j/ooa/Aircraft.java
@@ -5,5 +5,4 @@ public class Aircraft {
     public void printModel() {
         System.out.println("Метод выводит в консоль модель самолета.");
     }
-
 }
